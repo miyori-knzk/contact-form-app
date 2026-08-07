eb59f180fb6caa9d899b7f54231596b1a57ea98f
miyori-knzk <miyu.a_1022@icloud,com>
2026-08-07 14:37:00 +0900

    feat: 管理画面実装

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -32,9 +32,34 @@ class Contact extends Model
         };
     }
 
+    public static function makeQuery($request)
+    {
+        $query = static::query();
+
+        if ($request->filled('keyword')) {
+            $query->where('first_name', 'like', '%' . $request->keyword . '%')
+                ->orWhere('last_name', 'like', '%' . $request->keyword . '%')
+                ->orWhere('email', 'like', '%' . $request->keyword . '%');
+        }
+
+        if ($request->filled('gender')) {
+            $query->where('gender', '=', $request->gender);
+        }
+
+        if ($request->filled('category_id')) {
+            $query->where('category_id', '=', $request->category_id);
+        }
+
+        if ($request->filled('date')) {
+            $query->whereDate('created_at', '=', $request->date);
+        }
+
+        return $query;
+    }
+
     public function category(): BelongsTo
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class)->withDefault();
     }
 
     public function contactTags(): HasMany
