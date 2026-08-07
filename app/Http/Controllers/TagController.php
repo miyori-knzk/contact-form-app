2c6a610c80ca5d98b50183d9dc3bfd41f1cbfedf
miyori-knzk <miyu.a_1022@icloud,com>
2026-08-07 11:28:53 +0900

    feat: タグCRUD実装

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TagRequest;
+use App\Models\Tag;
+
+class TagController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(RagRequest $request)
+    {
+        Tag::create($request->validated());
+
+        return redirect()->route('admin.index')->with('success', 'タグを作成しました。');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tag $tag)
+    {
+        return view('admin.tags.edit', compact($tag));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(TagRequest $request, Tag $tag)
+    {
+        Tag::update($request->validated());
+
+        return redirect()->route('admin.index')->with('success', 'タグを更新しました。');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+
+        return redirect()->route('admin.index')->with('success', 'タグを削除しました。');
+    }
+}
