65fb56c3aa5f04c92dc3a2f2e5fe457536f926ca
miyori-knzk <miyu.a_1022@icloud,com>
2026-08-12 11:09:27 +0900

    feat: ログイン画面の日本語化

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '入力された情報は登録されていません。',
+    'password' => 'パスワードが間違っています。',
+    'throttle' => 'ログイン試行が多すぎます。 :seconds 秒後にもう一度試してください。',
+
+];
